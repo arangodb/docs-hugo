45a2188e546bb565517862327df7630e0a38fce7
dandimeo <daniele@arangodb.com>
2023-02-08 09:54:56 +0100

    #DOC-349 EXTRA restructure now creates well defined nav tree with well defined weights

--- migration-tools/migration/restructure.sh
+++ migration-tools/migration/restructure.sh
@@ -32,48 +32,38 @@ mv foxx-microservices developer
 touch introduction/_index.md deploying/_index.md operations/_index.md developer/_index.md _index.md
 
 echo "---
-fileID: transactions
 title: '3.10'
 weight: 0
 description: 
 layout: default
 ---" >> _index.md
 echo "---
-fileID: transactions
-title: Operations
-weight: 9000
-description: 
-layout: default
----" >> operations/_index.md
-echo "---
-fileID: transactions
-title: Developer topics
-weight: 10000
-description: 
-layout: default
----" >> developer/_index.md
-echo "---
-fileID: transactions
 title: Introduction to ArangoDB
 weight: 1000
 description: 
 layout: default
 ---" >> introduction/_index.md
 echo "---
-fileID: transactions
 title: Operations
 weight: 9000
 description: 
 layout: default
 ---" >> operations/_index.md
 echo "---
-fileID: transactions
 title: Developer topics
 weight: 10000
 description: 
 layout: default
 ---" >> developer/_index.md
 
+sed -i "s/weight: .*/weight: 2000/" arangograph/_index.md
+sed -i "s/weight: .*/weight: 3000/" aql/_index.md
+sed -i "s/title: .*/title: ArangoDB Query Language (AQL)/" aql/_index.md
+sed -i "s/weight: .*/weight: 4000/" graphs/_index.md
+sed -i "s/weight: .*/weight: 5000/" data-science/_index.md
+sed -i "s/weight: .*/weight: 6000/" transactions/_index.md
+sed -i "s/weight: .*/weight: 7000/" indexing/_index.md
+sed -i "s/title: .*/title: Indexing and Searching Data/" indexing/_index.md
 
 
 cd ../3.11
@@ -108,30 +98,36 @@ mv foxx-microservices developer
 touch introduction/_index.md deploying/_index.md operations/_index.md developer/_index.md _index.md
 
 echo "---
-fileID: transactions
 title: '3.11'
 weight: 0
 description: 
 layout: default
 ---" >> _index.md
 echo "---
-fileID: transactions
 title: Introduction to ArangoDB
 weight: 1000
 description: 
 layout: default
 ---" >> introduction/_index.md
 echo "---
-fileID: transactions
 title: Operations
 weight: 9000
 description: 
 layout: default
 ---" >> operations/_index.md
 echo "---
-fileID: transactions
 title: Developer topics
 weight: 10000
 description: 
 layout: default
 ---" >> developer/_index.md
+
+sed -i "s/weight: .*/weight: 2000/" arangograph/_index.md
+sed -i "s/weight: .*/weight: 3000/" aql/_index.md
+sed -i "s/title: .*/title: ArangoDB Query Language (AQL)/" aql/_index.md
+sed -i "s/weight: .*/weight: 4000/" graphs/_index.md
+sed -i "s/weight: .*/weight: 5000/" data-science/_index.md
+sed -i "s/weight: .*/weight: 6000/" transactions/_index.md
+sed -i "s/weight: .*/weight: 7000/" indexing/_index.md
+sed -i "s/title: .*/title: Indexing and Searching Data/" indexing/_index.md
+
