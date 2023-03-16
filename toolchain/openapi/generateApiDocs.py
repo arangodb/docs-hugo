57bcdebd39aabaf55242227bdccec081dbf650e9
Simran Spiller <simran@arangodb.com>
2023-03-16 17:14:21 +0100

    Reduce OpenAPI indentation from 4 to 2 spaces

--- toolchain/openapi/generateApiDocs.py
+++ toolchain/openapi/generateApiDocs.py
@@ -79,7 +79,7 @@ def processFile(filepath):
         apiDocsRes["paths"][path][method] = endpointDict["paths"][path][method]
 
     dstFile = open(dst, "w")
-    json.dump(apiDocsRes, dstFile, indent=4)
+    json.dump(apiDocsRes, dstFile, indent=2)
     dstFile.close()
 
 if __name__ == "__main__":
