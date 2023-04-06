44289d80697215e01aa12d7ba5ab5970d01176c2
dandimeo <daniele@arangodb.com>
2023-04-06 16:20:27 +0200

    toolchain script to start any kind of arangodb docker image and inject to arangoproxy, arangosh injection from arangodb image to arangoproxy container, dockerfile to create docker image from arangodb compiled tar and setup and entrypoint scripts for it, use of servers.yaml file to define servers and images to use in generation process

--- toolchain/scripts/toolchain.sh
+++ toolchain/scripts/toolchain.sh
@@ -8,34 +8,46 @@ function start_server() {
 
   echo "[START_SERVER] Setup server"
   echo "$name" "$image" "$version"
+  echo ""
 
+  echo "[START_SERVER] setup docs_net docker network"
   docker network inspect docs_net >/dev/null 2>&1 || docker network create --driver=bridge --subnet=192.168.129.0/24 docs_net
+  echo ""
 
+  echo "[START_SERVER] Cleanup old containers"
   docker container stop "$name" >/dev/null 2>&1
   docker container rm "$name" >/dev/null 2>&1
+  echo ""
 
-
-  mkdir -p ../.arangosh/"$name"/bin arangosh/"$name"/usr arangosh/"$name"/etc/relative
 
   echo "[START_SERVER] Run server"
-  docker run -e ARANGO_NO_AUTH=1 --net docs_net --name "$name" -d "$image" arangod \
-    --server.endpoint tcp://0.0.0.0:8529\
+  docker run -e ARANGO_NO_AUTH=1 --net docs_net --name "$name" -d "$image"
 
-  echo "[START_SERVER] Copy arango programs to arangoproxy volumes"
-  docker cp "$name":/usr/bin arangosh/"$name"/usr
-  docker cp "$name":/usr/share/arangodb3 arangosh/"$name"/share
-  docker cp "$name":/usr/share/doc arangosh/"$name"/share
-  docker cp "$name":/usr/share/man arangosh/"$name"/share
-  docker cp "$name":/etc/arangodb3/arangosh.conf arangosh/"$name"/etc/relative/arangosh.conf
+  echo "[START_SERVER] Setup dedicated arangosh in arangoproxy"
+  cd ../arangoproxy
+  rm -r arangosh
+
+  mkdir -p arangosh/"$name"/usr arangosh/"$name"/usr/bin arangosh/"$name"/usr/bin/etc/relative
+
+  docker cp "$name":/usr/bin/arangosh arangosh/"$name"/usr/bin/arangosh
+  docker cp "$name":/usr/bin/icudtl.dat arangosh/"$name"/usr/bin/icudtl.dat
+
+  docker cp "$name":/usr/share/ arangosh/"$name"/usr/
+  docker cp "$name":/etc/arangodb3/arangosh.conf arangosh/"$name"/usr/bin/etc/relative/arangosh.conf
+
+  sed -i -e 's~startup-directory.*~startup-directory = /home/arangoproxy/arangosh/'"$name"'/usr/share/arangodb3/js~' arangosh/"$name"/usr/bin/etc/relative/arangosh.conf
+  echo ""
 
   echo "[START_SERVER] Retrieve server ip"
   ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$name")
   echo "IP: "$ip""
+  echo ""
 
   printf -v url "http://%s:8529" $ip
 
   echo "[START_SERVER] Copy server configuration in arangoproxy repositories"
   yq e '.repositories += [{"name": "'"$name"'", "image": "'"$image"'", "version": "'"$version"'", "url": "'"$url"'"}]' -i ../arangoproxy/cmd/configs/local.yaml
+  echo "[START_SERVER] Done"
 }
 
 
