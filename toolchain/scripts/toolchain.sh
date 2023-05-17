49ff73bd1249d5b2c66dbbd433031546c02240e2
dandimeo <daniele@arangodb.com>
2023-05-17 23:36:30 +0200

    docker container ps -a in terminate script for debug purposes

--- toolchain/scripts/toolchain.sh
+++ toolchain/scripts/toolchain.sh
@@ -401,6 +401,7 @@ function trap_container_exit() {
     fi
   done
   echo "[TERMINATE] Before docker compose stop all" >> arangoproxy-log.log
+  docker container ps -a
   docker container stop $(docker ps -aq)
   echo "[TERMINATE] After docker container stop all" >> arangoproxy-log.log
   echo "[TERMINATE] After docker container stop all"
