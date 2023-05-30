36f6c1a8b3b1ec3576d99e3a11863ddeb4e6b519
dandimeo <64097057+dandimeo@users.noreply.github.com>
2023-05-30 12:32:59 +0200

    log PR body field (#37)
    * log PR body field
    
    * log body
    
    * first draft
    
    * pause enterprise clone
    
    * build arangoproxy and site images
    
    * generate able to compile if image does not exist before generating content
    
    * fix plain-build yaml
    
    * fix commit-generated parameter missing
    
    * use skopeo to check docker image exists
    
    * test how to exit 0 from docker inspect
    
    * test
    
    * set +e on docker image check
    
    * fix git clone param
    
    * add docker create image to generate
    
    * add exit 0
    
    * fix persist path in plai nbuild
    
    * git clone docs-hugo to build docker image
    
    * create-pr flag for scheduled generate
    
    * restore show-deploy-preview step
    
    * delete github app folder as it has its own repo
    
    * no devel default in arangodb-branch
    
    * no default in version param
    
    * use target_url as deploy preview link
    
    * extra cut in utils
    
    * check
    
    * wrong check on 0
    
    ---------
    
    Co-authored-by: dandimeo <daniele@arangodb.com>

--- .circleci/utils.sh
+++ .circleci/utils.sh
@@ -23,22 +23,22 @@ function generate_setup-arangodb-branches(){
     ENTERPRISE_BRANCH="$2"
 
     echo "[SETUP] Setup server $ARANGODB_BRANCH"
-    branch_name=$(echo $ARANGODB_BRANCH | cut -d= -f2 | cut -d, -f2)
+    branch_name=$(echo $ARANGODB_BRANCH | cut -d= -f2 | cut -d ' ' -f2 | cut -d, -f2)
     echo "$branch_name"
-    version=$(echo $ARANGODB_BRANCH | cut -d= -f2 | cut -d, -f3)
+    version=$(echo $ARANGODB_BRANCH | cut -d= -f2 | cut -d ' ' -f2 | cut -d, -f3)
 
     if [[ "$branch_name" == *"arangodb/enterprise"* ]]; then
         echo "[SETUP] An official ArangoDB Enterprise image has been chosen"
         preview_branch=$(echo $branch_name | cut -d: -f2 | cut -d- -f1)
         git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $preview_branch $preview_branch
-        clone-arangodb-enterprise $preview_branch $ENTERPRISE_BRANCH
+        # clone-arangodb-enterprise $preview_branch $ENTERPRISE_BRANCH
         echo "[SETUP] Pull Docker Image $branch_name"
         docker pull $branch_name
     else 
         echo "[SETUP] A Feature-PR Docker image has been choosen"
         image_name=$(echo ${branch_name##*/})
         git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $branch_name $image_name
-        clone-arangodb-enterprise $branch_name $ENTERPRISE_BRANCH
+        # clone-arangodb-enterprise $branch_name $ENTERPRISE_BRANCH
 
         main_hash=$(awk 'END{print}' $image_name/.git/logs/HEAD | awk '{print $2}' | cut -c1-9)
         echo "[SETUP] Check TAG Image arangodb/docs-hugo:$image_name-$version-$main_hash"
@@ -59,3 +59,4 @@ function generate_setup-environment-var-branch() {
         export ARANGODB_SRC=/home/circleci/project/$image_name
     fi
 }
+
--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,41 +1,17 @@
+<!-- pr-description -->
 ### Description
 
 *(Please describe the changes in this PR for reviewers, motivation, rationale - **mandatory**)*
 
-### CI
+<br>
+<br>
 
-*(Please use the following checkboxes to trigger the right CI)*
 
-The following PR deals with the following generated content:
-
-- [ ] Examples
-- [ ] Startup options
-- [ ] Metrics
-- [ ] API documentation
-- [ ] Error Codes
-
+<!-- pr-upstreams -->
+##### Upstream PR
 The following PR deals with the following generated content for the following versions:
 
-- [ ] 3.10  *(if selected, please specify the ArangoDB Branch to be used to generate the content)*
-  - ArangoDB Main Branch: 
-  - ArangoDB Enterprise Branch:
-- [ ] 3.11  *(if selected, please specify the ArangoDB Branch to be used to generate the content)*
-  - ArangoDB Main Branch: 
-  - ArangoDB Enterprise Branch:
-- [ ] 3.12  *(if selected, please specify the ArangoDB Branch to be used to generate the content)*
-  - ArangoDB Main Branch: 
-  - ArangoDB Enterprise Branch:
-- [ ] devel  *(if selected, please specify the ArangoDB Branch to be used to generate the content)*
-  - ArangoDB Main Branch: 
-  - ArangoDB Enterprise Branch:
-
-
-#### Upstream PRs
-
-*(Please reference the arangodb main PR)*
-
-- 3.10: 
+- 3.10:
 - 3.11:
 - 3.12:
 - devel:
-
