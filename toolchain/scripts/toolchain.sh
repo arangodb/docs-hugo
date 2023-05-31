4546a907537d3fbf5aa3befe50d8c0e8fbf9d20b
dandimeo <daniele@arangodb.com>
2023-06-01 00:04:27 +0200

    try using images from repo

--- toolchain/scripts/toolchain.sh
+++ toolchain/scripts/toolchain.sh
@@ -466,8 +466,16 @@ echo "[TOOLCHAIN] Generators: $GENERATORS"
 
   ## Start arangoproxy and site containers to build examples and site
   if [ "$generate_examples" = true ] ; then
+
+    if [ "$DOCKER_ENV" == "dev" ]; then
       docker build --target arangoproxy ../docker/ -t arangoproxy
       docker  build --target hugo ../docker/ -t site
+    else 
+      docker pull arangodb/docs-hugo:arangoproxy
+      docker pull arangodb/docs-hugo:site
+      docker tag arangodb/docs-hugo:arangoproxy arangoproxy
+      docker pull arangodb/docs-hugo:site site
+    fi
     
     cd ../../
     echo "[GENERATE-EXAMPLES]  Run arangoproxy and site containers"
