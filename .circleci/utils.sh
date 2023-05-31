071a405cfea466ea3fa0f190be7b5a8c4402f1db
dandimeo <daniele@arangodb.com>
2023-05-31 18:20:11 +0200

    fix clone enterprise logic

--- .circleci/utils.sh
+++ .circleci/utils.sh
@@ -2,20 +2,15 @@
 
 function clone-arangodb-enterprise() {
     ARANGODB_BRANCH="$1"
-    ENTERPRISE_BRANCH="$2"
-    if ["$ENTERPRISE_BRANCH" == ""]; then
-        set +e
-        git ls-remote --exit-code --heads git@github.com:arangodb/enterprise.git "$1"
-        if [ "$?" == "0" ] ; then
-            ENTERPRISE_BRANCH="$1"
-        else
-            ENTERPRISE_BRANCH=devel
-        fi
-        set -e
+    ENTERPRISE_BRANCH="devel"
+    set +e
+    git ls-remote --exit-code --heads git@github.com:arangodb/enterprise.git "$1"
+    if [ "$?" == "0" ] ; then
+        ENTERPRISE_BRANCH="$1"
     fi
+    set -e
     echo "Using enterprise branch $ENTERPRISE_BRANCH"
-    image_name=$(echo ${ARANGODB_BRANCH##*/})
-    git clone --depth 1 git@github.com:arangodb/enterprise.git --branch "$ENTERPRISE_BRANCH" $image_name/enterprise
+    git clone --depth 1 git@github.com:arangodb/enterprise.git --branch "$ENTERPRISE_BRANCH" $ARANGODB_BRANCH/enterprise
 }
 
 function generate_setup-arangodb-branches(){
@@ -31,14 +26,14 @@ function generate_setup-arangodb-branches(){
         echo "[SETUP] An official ArangoDB Enterprise image has been chosen"
         preview_branch=$(echo $branch_name | cut -d: -f2 | cut -d- -f1)
         git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $preview_branch $preview_branch
-        # clone-arangodb-enterprise $preview_branch $ENTERPRISE_BRANCH
+        clone-arangodb-enterprise $preview_branch
         echo "[SETUP] Pull Docker Image $branch_name"
         docker pull $branch_name
     else 
         echo "[SETUP] A Feature-PR Docker image has been choosen"
         image_name=$(echo ${branch_name##*/})
         git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $branch_name $image_name
-        # clone-arangodb-enterprise $branch_name $ENTERPRISE_BRANCH
+        clone-arangodb-enterprise $branch_name
 
         main_hash=$(awk 'END{print}' $image_name/.git/logs/HEAD | awk '{print $2}' | cut -c1-9)
         echo "[SETUP] Check TAG Image arangodb/docs-hugo:$image_name-$version-$main_hash"
