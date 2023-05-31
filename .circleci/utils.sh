4bee27628eeae64b23af797a67224a7d3c3dbd5c
dandimeo <daniele@arangodb.com>
2023-05-31 20:21:28 +0200

    fix export arangodb_branch_number

--- .circleci/utils.sh
+++ .circleci/utils.sh
@@ -60,7 +60,7 @@ function pull-branch-image(){
 function generate_setup-environment-var-branch() {
     ARANGODB_BRANCH="$1"
     branch_name=$(echo $ARANGODB_BRANCH | cut -d= -f2 | cut -d, -f2)
-    export ARANGODB_BRANCH=$ARANGODB_BRANCH
+    export ARANGODB_BRANCH"$2"=$ARANGODB_BRANCH
     if [[ "$branch_name" == *"arangodb/enterprise"* ]]; then
         preview_branch=$(echo $branch_name | cut -d: -f2 | cut -d- -f1)
         export ARANGODB_SRC"$2"=/home/circleci/project/$preview_branch
