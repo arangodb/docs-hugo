3c7439a514b11eabc399a13c94bf8e6983eb802e
dandimeo <daniele@arangodb.com>
2023-06-15 23:39:30 +0200

    node script to get artifacts url of previous generate run on same branch

--- .circleci/commit.js
+++ .circleci/commit.js
@@ -1,10 +1,14 @@
 
+const fs = require("fs");
+const { exit } = require("process");
+const request = require("request")
+const args = process.argv.slice(2);
 
 async function commit_generated() {
 
-    let response = await fetch('https://circleci.com/api/v2/insights/gh/arangodb/docs-hugo/workflows/generate?branch=demo-uc-1.b.i.1', {
+    let response = await fetch('https://circleci.com/api/v2/insights/gh/arangodb/docs-hugo/workflows/generate?branch='+args[0], {
         method: 'GET',
-        headers: {'content-type': 'application/json', 'Circle-Token': "136e3ef3543343df93136774955b443dd8d77ca8"},
+        headers: {'content-type': 'application/json', 'Circle-Token': process.env.CIRCLE_OIDC_TOKEN_V2},
     })
     
     let data = await response.json();
@@ -17,23 +21,25 @@ async function commit_generated() {
 
         let jobs = await fetch('https://circleci.com/api/v2/workflow/'+workflow.id+'/job', {
             method: 'GET',
-            headers: {'content-type': 'application/json', 'Circle-Token': "136e3ef3543343df93136774955b443dd8d77ca8"},
+            headers: {'content-type': 'application/json', 'Circle-Token': process.env.CIRCLE_OIDC_TOKEN_V2},
         })
         
         let jobsData = await jobs.json();
         for (let job of jobsData.items) {
             if (job.name != "build-with-generated") continue
-            console.log(job.job_number)
             let artifacts = await fetch('https://circleci.com/api/v2/project/gh/arangodb/docs-hugo/'+job.job_number+'/artifacts', {
                 method: 'GET',
-                headers: {'content-type': 'application/json', 'Circle-Token': "136e3ef3543343df93136774955b443dd8d77ca8"},
+                headers: {'content-type': 'application/json', 'Circle-Token': process.env.CIRCLE_OIDC_TOKEN_V2},
             })
 
             let artifactsData = await artifacts.json();
-            console.log(artifactsData)
+            if (artifactsData.items.length == 0) continue
+            for (let artifact of artifactsData.items) {
+                console.log(artifact.url)
+            }
+            exit()
         }
     }
-    return data;
 }
 
-commit_generated().then(console.log("ok")).catch(console.log("no"))
+commit_generated().then().catch()
