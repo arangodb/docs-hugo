3d1cfac9ec6e44c9eb32ef7719252f1d395f9e1a
dandimeo <64097057+dandimeo@users.noreply.github.com>
2023-07-14 23:40:45 +0200

    Improve CircleCI (#70)
    * try improve generate pipeline
    
    * fix root workspace
    
    * fix root workspace
    
    * fix clone-branch
    
    * add echo git clone
    
    * rm root project content
    
    * try persist workspace in between steps
    
    * persist only version folder
    
    * strip project folder from cp
    
    * change export ARANGODB_SRC
    
    * np
    
    * np
    
    * source utils in create docker image
    
    * pwd and ls + cd to create-docker
    
    * chmod +x on docker setup
    
    * fix chmod
    
    * fix pull-branch-image for non digit branch name
    
    * try using * to copy hidden files
    
    * fix cp -r
    
    * add docs_branch in curl docker files
    
    * fix awk .git
    
    * fix clone-btranch
    
    * add missing fi
    
    * add npm upgrade
    
    * update cimg node deploy
    
    ---------
    
    Co-authored-by: dandimeo <daniele@arangodb.com>

--- .circleci/utils.sh
+++ .circleci/utils.sh
@@ -2,7 +2,6 @@
 
 function clone-arangodb-enterprise() {
     BRANCH="$1"
-    FOLDER="$2"
     ENTERPRISE_BRANCH="devel"
     set +e
     git ls-remote --exit-code --heads git@github.com:arangodb/enterprise.git "$1"
@@ -11,25 +10,56 @@ function clone-arangodb-enterprise() {
     fi
     set -e
     echo "Using enterprise branch $ENTERPRISE_BRANCH"
-    git clone --depth 1 git@github.com:arangodb/enterprise.git --branch "$ENTERPRISE_BRANCH" $FOLDER/enterprise
+    git clone --depth 1 git@github.com:arangodb/enterprise.git --branch "$ENTERPRISE_BRANCH" /root/project/enterprise
 }
 
 function clone-branch() {
     BRANCH="$1"
+    VER="$2"
+    branch_name="$1"
 
     echo "[SETUP] Setup server $BRANCH"
 
     if [[ "$BRANCH" == *"arangodb/enterprise"* ]]; then
         echo "[SETUP] An official ArangoDB Enterprise image has been chosen"
-        preview_branch=$(echo $BRANCH | cut -d: -f2 | cut -d- -f1)
-        git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $preview_branch $preview_branch
-        clone-arangodb-enterprise $preview_branch $preview_branch
-    else 
-        echo "[SETUP] A Feature-PR Docker image has been choosen"
-        image_name=$(echo ${BRANCH##*/})
-        git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $BRANCH $image_name
-        clone-arangodb-enterprise $BRANCH $image_name
-    fi 
+        branch_name=$(echo $BRANCH | cut -d: -f2 | cut -d- -f1)
+    fi
+
+    echo "[SETUP] Git clone $branch_name"
+
+    git clone --depth 1 https://github.com/arangodb/arangodb.git --branch $branch_name /root/project
+    clone-arangodb-enterprise $branch_name
+
+    mkdir -p /tmp/$VER
+    cp -r /root/project/. /tmp/$VER
+}
+
+function create-docker-image() {
+    BRANCH="$1"
+    VER="$2"
+    DOCS_BRANCH="$3"
+
+    mkdir -p create-docker/
+
+    curl "https://raw.githubusercontent.com/arangodb/docs-hugo/$DOCS_BRANCH/toolchain/scripts/compile/tar-to-docker.Dockerfile" > create-docker/tar-to-docker.Dockerfile
+    curl "https://raw.githubusercontent.com/arangodb/docs-hugo/$DOCS_BRANCH/toolchain/scripts/compile/setup-tar-to-docker.sh" > create-docker/setup-tar-to-docker.sh
+    curl "https://raw.githubusercontent.com/arangodb/docs-hugo/$DOCS_BRANCH/toolchain/scripts/compile/docker-entrypoint.sh" > create-docker/docker-entrypoint.sh
+
+    mv install.tar.gz create-docker/
+
+    main_hash=$(awk 'END{print}' .git/logs/HEAD | awk '{print $2}' | cut -c1-9)
+    image_name=$(echo $BRANCH | cut -d/ -f2)
+
+    cd create-docker
+    chmod +x setup-tar-to-docker.sh
+    chmod +x docker-entrypoint.sh
+
+    apk add docker-cli
+    
+    docker build -t arangodb/docs-hugo:$image_name-$VER-$main_hash --target arangodb-tar-starter -f tar-to-docker.Dockerfile .
+
+    echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
+    docker push arangodb/docs-hugo:$image_name-$VER-$main_hash
 }
 
 function pull-branch-image(){
@@ -45,7 +75,7 @@ function pull-branch-image(){
     else 
         echo "[SETUP] A Feature-PR Docker image has been choosen"
         image_name=$(echo ${BRANCH##*/})
-        main_hash=$(awk 'END{print}' $image_name/.git/logs/HEAD | awk '{print $2}' | cut -c1-9)
+        main_hash=$(awk 'END{print}' $version/.git/logs/HEAD | awk '{print $2}' | cut -c1-9)
         echo "[SETUP] Check TAG Image arangodb/docs-hugo:$image_name-$version-$main_hash"
         docker pull arangodb/docs-hugo:$image_name-$version-$main_hash
         docker tag arangodb/docs-hugo:$image_name-$version-$main_hash $image_name-$version
@@ -56,14 +86,8 @@ function pull-branch-image(){
 
 function generate_setup-environment-var-branch() {
     BRANCH="$1"
-    version="$2"
+    version="$(echo "$2" | tr _ .)"
     export ARANGODB_BRANCH_"$2"=$BRANCH
-    if [[ "$BRANCH" == *"arangodb/enterprise"* ]]; then
-        preview_branch=$(echo $BRANCH | cut -d: -f2 | cut -d- -f1)
-        export ARANGODB_SRC_"$2"=/home/circleci/project/$preview_branch
-    else
-        image_name=$(echo ${BRANCH##*/})
-        export ARANGODB_SRC_"$2"=/home/circleci/project/$image_name
-    fi
+    export ARANGODB_SRC_"$2"=/home/circleci/project/$version
 }
 
