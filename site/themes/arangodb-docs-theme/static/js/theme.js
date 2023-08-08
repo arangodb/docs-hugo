2f69d42cbd842b98e102cbc9dee7e5dffe34c22f
dandimeo <daniele@arangodb.com>
2023-08-08 10:38:50 +0200

    revert aliases

--- site/themes/arangodb-docs-theme/static/js/theme.js
+++ site/themes/arangodb-docs-theme/static/js/theme.js
@@ -39,7 +39,6 @@ function menuEntryClickListener() {
 function renderVersion() {
     var version = localStorage.getItem('docs-version');
     var menuEntry = document.getElementsByClassName('version-menu');
-    console.log(menuEntry)
     for ( let entry of menuEntry ) {
         if (entry.classList.contains(version)) {
             entry.style.display = 'block';
@@ -130,7 +129,6 @@ function styleImages() {
 
 function loadPage(target) {
   var href = target;
-  console.log(target)
   renderVersion();
   loadMenu(href);
   $.get({
@@ -236,11 +234,12 @@ $(window).scroll(function(){
 
 */
 
+var stableVersion;
 
 function getCurrentVersion() {
     var url = window.location.href;
     var urlRe = url.match("\/[0-9.]+\/")
-    var urlVersion = "stable";
+    var urlVersion = stableVersion;
 
     if (urlRe) {
         urlVersion = urlRe[0].replaceAll("\/", "");
