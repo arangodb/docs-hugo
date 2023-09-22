fd081de1dbd94c94bff405234fee508d039eb8da
dandimeo <daniele@arangodb.com>
2023-09-22 15:25:38 +0200

    add override flag to arangoproxy

--- toolchain/arangoproxy/cmd/main.go
+++ toolchain/arangoproxy/cmd/main.go
@@ -10,12 +10,13 @@ import (
 )
 
 var configFile string
-var env string
+var env, override string
 var help, useServers bool
 
 // Pre-Run Setup
 func init() {
 	flag.StringVar(&configFile, "config", "./configs/local.yaml", "path of config file")
+	flag.StringVar(&override, "override", " ", "override examples")
 	flag.BoolVar(&help, "help", false, "Display help usage")
 	flag.BoolVar(&useServers, "use-servers", false, "Enable communication with arangodb servers")
 	flag.Parse()
@@ -25,6 +26,7 @@ func init() {
 		fmt.Printf("Error loading config: %s\n, aborting...", err.Error())
 		os.Exit(1)
 	}
+	models.Conf.Override = override
 
 	models.Logger.Printf(startupBanner)
 	models.Logger.Printf("./arangoproxy -help for help usage\n\n")
@@ -34,6 +36,7 @@ func init() {
 		models.Logger.Printf("Usage: ...\n")
 		os.Exit(0)
 	}
+	models.Logger.Printf("Configuration:\n%v\n", models.Conf)
 
 	models.Logger.Printf("Setup Done\n---------\n")
 
